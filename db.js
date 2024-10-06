6b53c398cc31fb8a5fb6132532881b661a4751ef
Aman308 <116431870+Aman308@users.noreply.github.com>
2024-10-06 20:59:57 +0530

    mongo updated

--- db.js
+++ db.js
@@ -1,9 +1,22 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/notex"
+// const mongoURI = "mongodb://localhost:27017/notex"
 
-const connectToMongo = () =>{
-    mongoose.connect(mongoURI).then(()=>console.log("Connected to Mongo Sucessfully")).catch((e)=>console.log(e.message))
+// const connectToMongo = () =>{
+//     mongoose.connect(mongoURI).then(()=>console.log("Connected to Mongo Sucessfully")).catch((e)=>console.log(e.message))
+// }
+
+// module.exports = connectToMongo;
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const connectDB = async () =>{
+    // console.log(process.env.MONGO_URI)
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connect to MongoDB successfull")
+    } catch (error) {
+        console.log("failed " + error)
+    }
 }
-
-module.exports = connectToMongo;
+module.exports = connectDB
--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 app.use(cors())
 app.use(express.json())
